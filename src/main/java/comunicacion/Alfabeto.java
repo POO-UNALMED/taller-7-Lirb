4c0e298d596cbf48004084f38ea8e30d042da9c5
Antonio José Moreno Saavedra <ajmorenos@unal.edu.co>
2020-10-11 10:37:57 -0500

    Alfabeto extends de Pictograma

--- src/main/java/comunicacion/Alfabeto.java
+++ src/main/java/comunicacion/Alfabeto.java
@@ -3,9 +3,15 @@ package comunicacion;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Alfabeto {
+public class Alfabeto extends Pictograma{
     private List<String> letras = new ArrayList<String>();
     private String interpretacion = "";
+
+    public Alfabeto(String origen, String interpretacion, List<String> letras){
+        super(origen);
+        this.interpretacion = interpretacion;
+        this.letras = letras;
+    }
     public int cantidadLetras(){
         return this.letras.size();
     }
